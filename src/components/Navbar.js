c44bf8df97a98f0f9143fe8ce1f66e88506ad373
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2023-05-12 13:06:54 +0200

    basic routes and navbar set up done

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className="toggleButton">
+        <button></button>
+      </div>
+      <div className='Links'></div>
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+    </div>
+  )
+}
+
+export default Navbar
