88cd18266540abe21969381e6fbc2171e9532805
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2023-05-16 11:21:33 +0200

    footer element, projects page and individual project display first version completed. Routing is working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import InstagramIcon from "@material-ui/icons/Instagram";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import LinkedinIcon from "@material-ui/icons/LinkedIn";
+import "../styles/Home.css"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='about'>
+        <h2>Hi, my name is Jeronimo</h2>
+        <div className='prompt'>
+          <p>A designer and architect</p>
+          <InstagramIcon />
+          <FacebookIcon />
+          <LinkedinIcon />
+        </div>
+      </div>
+      <div className='skills'></div>
+    </div>
   )
 }
 
