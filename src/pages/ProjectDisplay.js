72957d972c6e6518118cfa72a47a86a93300afe9
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2023-06-07 11:25:15 +0200

    image slider working but there still a lot of frontend work to be done

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import {React, useState} from 'react'
 import { useParams } from "react-router-dom";
 import { ProjectList } from '../helpers/ProjectList';
 import "../styles/ProjectDisplay.css";
 
 function ProjectDisplay() {
   const { id } = useParams();
+
   const project = ProjectList[id];
+
+  const slides = project.images.map(image => {
+    return image;
+  });
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
   return (
     <div className='project'>
       <div className='project-info'>
@@ -14,7 +38,25 @@ function ProjectDisplay() {
         <h3> Client: {project.client}</h3>
         <p> {project.description}</p>
       </div>
-      <img src={project.images[0]}/>
+      <div className='slides-container'>
+        <div className='arrow-container'>
+          <div className='right-arrow' onClick={nextSlide} >❱</div>
+          <div className='left-arrow' onClick={prevSlide} >❰</div>
+        </div>
+        <div className=''><img src={slides[currentIndex]}/></div>
+        <div className='dots-container'>
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className='dot'
+          >
+            ●
+          </div>
+        ))}
+        </div>
+      </div>
+
     </div>
   )
 }
