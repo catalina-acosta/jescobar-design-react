e5fd4794242f5ab3005454c76377118bc23069ad
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2023-09-20 11:01:30 +0200

    links added and working

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import InstagramIcon from "@material-ui/icons/Instagram";
 import FacebookIcon from "@material-ui/icons/Facebook";
-import LinkedinIcon from "@material-ui/icons/LinkedIn";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import "../styles/Footer.css";
 
 function Footer() {
@@ -10,8 +10,20 @@ function Footer() {
       <p> &copy; Jeronimo Escobar All rights reserved </p>
 
       <div className='socialmedia'>
-        <InstagramIcon />
-        <LinkedinIcon />
+        <a
+          href='https://instagram.com/jescobar.design?igshid=NTc4MTIwNjQ2YQ=='
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <InstagramIcon />
+        </a>
+        <a
+          href='https://www.linkedin.com/in/jeronimo-escobar-barreto-3075728a/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <LinkedInIcon />
+        </a>
       </div>
       <p className='email'>email: jeronimoescobarb@gmail.com</p>
     </div>
