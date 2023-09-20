8c472f7b2c35ff83ec8cc34fbcf7b97618077907
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2023-09-20 12:43:25 +0200

    effect added for landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,48 @@
-import React from 'react'
-import InstagramIcon from "@material-ui/icons/Instagram";
-import FacebookIcon from "@material-ui/icons/Facebook";
-import LinkedinIcon from "@material-ui/icons/LinkedIn";
-import "../styles/Home.css"
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/Home.css';
 
 function Home() {
+  const navigate = useNavigate();
+  const [displayText, setDisplayText] = useState('');
+  const [showText, setShowText] = useState(false);
+
+  useEffect(() => {
+    const redirectToProjects = () => {
+      navigate('/projects');
+    };
+
+    const text = 'Jeronimo Escobar Design';
+
+    const displayTextInterval = setInterval(() => {
+      if (text.length > displayText.length) {
+        setDisplayText(text.substring(0, displayText.length + 1));
+      } else {
+        clearInterval(displayTextInterval);
+        setShowText(true);
+      }
+    }, 150);
+
+    if (showText) {
+      window.addEventListener('mousedown', redirectToProjects);
+      window.addEventListener('touchstart', redirectToProjects);
+      return () => {
+        window.removeEventListener('mousedown', redirectToProjects);
+        window.removeEventListener('touchstart', redirectToProjects);
+      };
+    }
+
+    return () => clearInterval(displayTextInterval);
+  }, [displayText, showText, navigate]);
+
   return (
     <div className='home'>
-      <div className='about'>
-        <h2>Hi, my name is Jeronimo</h2>
-        <div className='prompt'>
-          <p>A designer and architect</p>
-          <InstagramIcon />
-          <FacebookIcon />
-          <LinkedinIcon />
-        </div>
+      <div className='white-background'>
+        <h2>{displayText}</h2>
       </div>
       <div className='skills'></div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
