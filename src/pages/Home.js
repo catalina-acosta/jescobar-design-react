dd8f49fb78f99fed03e8de1c58e20d2de317fffd
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2024-02-06 09:32:57 +0100

    logo home page centered

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -39,7 +39,7 @@ function Home() {
   return (
     <div className='home'>
       <div className='white-background'>
-        <h1 className='logo'>{displayText}</h1>
+        <h1 className='logo-home'>{displayText}</h1>
       </div>
       <div className='skills'></div>
     </div>
